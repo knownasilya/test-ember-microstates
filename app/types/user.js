f660e00e90300ac3cdbb0a1d1b050300070b4153
Ilya Radchenko <knownasilya@gmail.com>
2019-01-15 18:02:25 -0500

    feat: attempt validations

--- app/types/user.js
+++ app/types/user.js
@@ -1,4 +1,28 @@
+import { validate } from 'ember-validators';
+
 export default class User {
-  name = String;
+  name = Field(String, [
+    ['presence', { presence: true }]
+  ]);
   isAdmin = Boolean;
+}
+
+export function Field(Type, validationArr) {
+  return class Field extends Type {
+    get validation() {
+      let value = valueOf(this);
+      let results = validationArr.map((check) => {
+        let [type, options] = check;
+
+        return validate(type, value, options);
+      });
+
+      debugger;
+      return results;
+    }   
+    get hasErrors() {
+      debugger;
+      return this.validation.length > 0;
+    }
+  }
 }
