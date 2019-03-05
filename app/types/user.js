e74af0d9b559e0a68d75aaf22e6204de3ce1c3a2
Ilya Radchenko <knownasilya@gmail.com>
2019-03-05 00:18:27 -0500

    Wire up validations a bit more

--- app/types/user.js
+++ app/types/user.js
@@ -1,22 +1,31 @@
 import { Primitive } from 'microstates/dist/microstates.cjs';
 import { validate } from 'ember-validators';
+import messages from 'ember-validators/messages';
 
 export default class User {
   name = Field([
-    ['presence', { presence: true }]
+    ['presence', { presence: true }],
+    ['length', { allowNone: false, min: 3 }]
   ]);
   isAdmin = Boolean;
 }
 
 export function Field(validationArr) {
   return class Field extends Primitive {
-
     get validation() {
       let value = this.state;
       let results = validationArr.map((check) => {
         let [type, options] = check;
-
-        return validate(type, value, options);
+	let opts = { ...options };
+	
+	if (!opts.description) {
+	  opts.description = this.constructor.path[0]
+	}
+        let result = validate(type, value, options, {}, this.constructor.path[0]);
+	if (result && result.type) {
+	  result.message = messages.getMessageFor(result.type, opts);
+	}
+        return result;
       });
 
       return results;
