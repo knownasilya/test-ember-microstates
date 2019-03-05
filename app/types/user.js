1502f935acf2eca9f2a9a8d0934e75376fbf4dcb
Charles Lowell <cowboyd@frontside.io>
2019-03-04 17:52:25 -0700

    Don't extend `String` since it's really `StringType` at runtime (#1)
    In order to make microstates convenient, `Number`, `String`, `Boolean`
    are actually standins for the microstate types `NumberType`,
    `StringType`, and `BooleanType` when specifying them as child
    properties:
    
    ```javascript
    class MyThing {
      num = Number
      str = String
    }
    ```
    
    this gets expanded to:
    
    ```javascript
    class MyThing {
      num = NumberType
      str = StringType
    }
    ```
    
    We can't do the same thing (or at least not very easily) with
    inheritance, so what was happening was that when the generated field
    `class Field extends String` constructor was being invoked, it was
    invoking the super String constructor which returned an instance of
    `String` "undefined", or "[object Object]" depending on the value that
    got passed in. This in turn was messing up the child lookup because
    microstates thought there was a child for each character in the
    string.
    
    This removes the inheritance (maybe containment is a better strategy)
    although it could be added back by exmplicitly `import { StringType }
    from 'microstates'` and descending from _that_ class.

--- app/types/user.js
+++ app/types/user.js
@@ -1,28 +1,28 @@
+import { Primitive } from 'microstates/dist/microstates.cjs';
 import { validate } from 'ember-validators';
 
 export default class User {
-  name = Field(String, [
+  name = Field([
     ['presence', { presence: true }]
   ]);
   isAdmin = Boolean;
 }
 
-export function Field(Type, validationArr) {
-  return class Field extends Type {
+export function Field(validationArr) {
+  return class Field extends Primitive {
+
     get validation() {
-      let value = valueOf(this);
+      let value = this.state;
       let results = validationArr.map((check) => {
         let [type, options] = check;
 
         return validate(type, value, options);
       });
 
-      debugger;
       return results;
-    }   
+    }
     get hasErrors() {
-      debugger;
-      return this.validation.length > 0;
+      return !!this.validation.some(result => result !== true);
     }
   }
 }
